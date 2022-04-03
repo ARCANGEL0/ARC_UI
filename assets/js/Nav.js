b5f544e9593a3f79cb4a77273b0611aa1929b29d
ARCANGEL0 <henrique@arcangelo.dev>
2022-04-02 21:51:30 -0300

    t

--- assets/js/Nav.js
+++ assets/js/Nav.js
@@ -2,30 +2,6 @@ $(document).ready(function () {
     var keypress = new Audio("assets/audio/keyenter.wav");
 
 
-    // Click function for show the Modal
-
-    $(".show").on("click", function () {
-        $(".mask").addClass("active");
-    });
-
-    // Function for close the Modal
-
-    function closeModal() {
-        $(".mask").removeClass("active");
-    }
-
-    // Call the closeModal function on the clicks/keyboard
-
-    $(".close, .mask").on("click", function () {
-        closeModal();
-    });
-
-    $(document).keyup(function (e) {
-        if (e.keyCode == 27) {
-            closeModal();
-        }
-    });
-
 
 
     $('#slide1').on('click', function () {
